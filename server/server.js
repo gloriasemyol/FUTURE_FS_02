56b7d48d580834c508d96888c47169c16de4fc0c
Gloria Semyol <gloriasemyol@gmail.com>
2026-07-28 17:46:40 +0530

    Day 5: restrict CORS to CLIENT_URL

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const leadRoutes = require('./routes/leadRoutes');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(express.json());
 
 // Test route
